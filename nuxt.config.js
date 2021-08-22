367c3f77bc56d26999365463164e746f26c9f53b
burirom <burirom1002@gmail.com>
2021-08-22 22:15:49 +0900

    update mail

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["~/plugins/vee-validate"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -66,5 +66,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ["vee-validate/dist/rules"],
+  },
 };
--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,53 @@
         <VuetifyLogo />
       </v-card>
       <v-card class="pa-6 my-4">
-        <form name="contact" method="POST" data-netlify="true">
+        <v-card-title> テストフォーム </v-card-title>
+        <validation-observer
+          ref="observer"
+          tag="form"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          @submit.prevent="onSubmit"
+        >
           <input type="hidden" name="form-name" value="contact" />
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|max:100"
+            name="お名前"
+          >
+            <v-text-field
+              v-model="name"
+              label="お名前"
+              required
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required|email|max:256"
+            name="メールアドレス"
+          >
+            <v-text-field
+              v-model="mail"
+              label="E-mail"
+              required
+              :error-messages="errors"
+            ></v-text-field>
+          </validation-provider>
 
-          <v-text-field v-model="name" label="お名前" required></v-text-field>
-          <v-text-field v-model="mail" label="E-mail" required></v-text-field>
           <v-text-field v-model="tel" label="電話番号" required></v-text-field>
-          <v-btn class="mr-4" type="submit"> 送信 </v-btn>
-        </form>
+
+          <v-btn
+            class="mr-4"
+            type="submit"
+            :loading="sendStatus === 'is-sending'"
+          >
+            送信
+          </v-btn>
+        </validation-observer>
+        <div>{{ text }}</div>
       </v-card>
     </v-col>
   </v-row>
@@ -24,10 +63,44 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
+      text: "",
       name: "",
       mail: "",
       tel: "",
+      botField: "",
+      sendStatus: "" as "" | "is-sending" | "is-error" | "is-complete",
     };
+  },
+  methods: {
+    onSubmit() {
+      if (this.sendStatus !== "is-sending") return;
+      this.sendStatus = "is-sending";
+      this.text = "送信中...";
+      const params = new URLSearchParams();
+      params.append("form-name", "contact");
+      params.append("username", this.name);
+      params.append("email", this.mail);
+      params.append("tel", this.tel);
+      if (this.botField) {
+        params.append("bot-field", this.botField);
+      }
+      this.$axios
+        .$post("/", params)
+        .then(() => {
+          this.text = "お問い合わせを送信しました！";
+          this.resetForm();
+          this.sendStatus = "is-complete";
+        })
+        .catch(() => {
+          this.text = "お問い合わせの送信が失敗しました";
+          this.sendStatus = "is-error";
+        });
+    },
+
+    resetForm() {
+      this.sendStatus = "";
+      this.$refs.observer.reset();
+    },
   },
 });
 </script>
